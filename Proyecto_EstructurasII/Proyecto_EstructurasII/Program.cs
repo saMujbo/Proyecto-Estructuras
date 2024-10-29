274222d26e2b1b121f155f12b03326d8d0dd8fc2
saMu <149215905+saMujbo@users.noreply.github.com>
2024-10-28 20:22:59 -0600

    Add files via upload

---
+++ Proyecto_EstructurasII/Proyecto_EstructurasII/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_EstructurasII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
